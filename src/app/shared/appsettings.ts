73cd1f4ed5a173ff0029f96940ba6bf3fc9ee7b6
Aidan Wolfheart <aidan@wolfheart.dev>
2023-04-28 02:22:39 +0100

    Fixed display of script ID and added error catching on conversation

--- src/app/shared/appsettings.ts
+++ src/app/shared/appsettings.ts
@@ -1,6 +1,6 @@
 export class AppSettings  {
     private static API_ENDPOINT='https://ezy.works/api/'
-    //private static API_ENDPOINT='http://127.0.0.1/'
+    //private static API_ENDPOINT='http://127.0.0.1:5000/'
 
     public static getEndpointWithService(serviceName: string) : string{
         return AppSettings.API_ENDPOINT + serviceName + '/';
